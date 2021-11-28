4b0732a8a0de5f8005fd9ecb2566cf08fd6b15fe
helder-ribeiro <helder.ribeiro@lnbio.cnpem.br>
2021-11-28 16:39:53 -0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -458,7 +458,7 @@ pub fn run<'a>(th: &'a str, ma: &'a str, se: &'a str) -> &'a Vec<&'a str> {
 
     for m in matrices {
         let scores =  m.scan(&seq);
-        results = scores
+        results = scores;
     }
     results
 }
